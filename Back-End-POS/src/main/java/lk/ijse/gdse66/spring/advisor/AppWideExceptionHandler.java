1eeed60c5c65625198f65833f03feb679ea450ba
ravindudinushan <ravindudinushan9966@gmail.com>
2024-03-09 18:49:17 +0530

    packaging

---
+++ Back-End-POS/src/main/java/lk/ijse/gdse66/spring/advisor/AppWideExceptionHandler.java
@@ -0,0 +1,17 @@
+package lk.ijse.gdse66.spring.advisor;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@CrossOrigin
+public class AppWideExceptionHandler {
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler({RuntimeException.class})
+    public ResponseUtil handleMyExceptions(RuntimeException e) {
+        return new ResponseUtil("Error", e.getMessage(), null);
+    }
+}
--- Back-End-POS/src/main/java/lk/ijse/gdse66/spring/config/WebAppConfig.java
+++ Back-End-POS/src/main/java/lk/ijse/gdse66/spring/config/WebAppConfig.java
@@ -1,4 +1,12 @@
 package lk.ijse.gdse66.spring.config;
 
+import lk.ijse.gdse66.spring.advisor.AppWideExceptionHandler;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackageClasses = {AppWideExceptionHandler.class}, basePackages = "lk.ijse.gdse66.spring.controller")
 public class WebAppConfig {
 }
