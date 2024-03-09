fd354f30e685bd71173e01dcb162e6ce803d717d
ravindudinushan <ravindudinushan9966@gmail.com>
2024-03-09 19:11:09 +0530

    packaging

--- Back-End-POS/src/main/java/lk/ijse/gdse66/spring/util/ResponseUtil.java
+++ Back-End-POS/src/main/java/lk/ijse/gdse66/spring/util/ResponseUtil.java
@@ -1,4 +1,14 @@
 package lk.ijse.gdse66.spring.util;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ResponseUtil {
+    private String state;
+    private String message;
+    private Object data;
 }
